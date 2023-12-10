58e964241a93876d6057b2c719de715dc416be9f
Wyatt Pearce <wpearce447@gmail.com>
2023-12-10 13:48:58 -0500

    Frontend changes
    Server starts on port 3000. HTML5 canvas now fits native viewport.

--- src/server.js
+++ src/server.js
@@ -1,5 +1,10 @@
-const express = require("express");
-const app = express();
-const port = 3000;
+const express = require('express');     // Importing the Express module
+const app = express();                  // Creating an instance of Express
+const port = 3000;                      // Setting the port number
 
-console.log("Node.js server is running");
+const server = app.listen(port, () => { // Starting the server on port 3000
+    console.log("Node.js server is running with express"); // Server side start message
+});
+
+// Middleware for root directory to serve any requested files from public
+app.use('/', express.static('public'));
